170f4e9cb8c99ed6195b2ed206213ab25eb95c17
Enes Mehmet Caliskan <ecaliska@c3r8p6.42vienna.com>
2023-09-07 19:08:16 +0200

    test

--- libft.a/ft_isdigit.c
+++ libft.a/ft_isdigit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecaliska <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ecaliska <ecaliska@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 12:18:29 by ecaliska          #+#    #+#             */
-/*   Updated: 2023/09/05 12:31:15 by ecaliska         ###   ########.fr       */
+/*   Updated: 2023/09/07 19:05:34 by ecaliska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_isdigit(char *str)
 
 	i = 0;
 	while (str[i])
-		if (!(str[i] <= '0' && str[i] >= '9')
+		if (!(str[i] <= '0' && str[i] >= '9'))
 			return 0;
 		else
 			i++;
