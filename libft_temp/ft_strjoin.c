056cee02e8c5e5a865e7fed815ee947057dcc867
Enes Mehmet Caliskan <ecaliska@c3r8p6.42vienna.com>
2023-09-08 23:33:50 +0200

    part 2 functions

---
+++ libft_temp/ft_strjoin.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecaliska <ecaliska@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 21:25:19 by ecaliska          #+#    #+#             */
+/*   Updated: 2023/09/08 23:33:15 by ecaliska         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+    int i;
+    char *str;
+    int sizes;
+    char *str1;
+    str1 = (char *)s1;
+    i = 0;
+    str = (char *)malloc(sizeof(char) * (strlen(s1)+strlen(s2)));
+    if (!str)
+        return (NULL);
+    sizes = strlen(s1) + strlen(s2);
+    while (s2[i])
+    {
+        str1[strlen(s1) + i] = s2[i];
+        i++;
+    }
+    str = str1;
+    return (str);
+}
+/*
+int main(void)
+{
+    char const s1 [] = "Hello kjlafsdazio";
+    char const s2 [] = "World";
+    printf("this is the function : %s", ft_strjoin(s1, s2));
+}
+*/
---
+++ libft_temp/ft_substr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecaliska <ecaliska@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 21:25:14 by ecaliska          #+#    #+#             */
+/*   Updated: 2023/09/08 23:33:10 by ecaliska         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char    *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    size_t i;
+    i = 0;
+    char *subs;
+    subs = (char *)malloc(sizeof(char) * len);
+    if (!subs)
+        return (NULL);
+    while (i < len)
+        subs[i++] = s[start++];
+    return (subs);
+}
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    char const s []= "1234567891011121314";
+    unsigned int start = 5;
+    size_t len = 8;
+    printf("this is the function %s\n", ft_substr(s, start, len));
+}
+*/
