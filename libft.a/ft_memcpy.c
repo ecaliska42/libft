f25c2c82af9b27cc0d0d1e4253d91df251029f24
ecaliska42 <140746793+ecaliska42@users.noreply.github.com>
2023-09-15 19:27:07 +0000

    francinette

--- libft.a/ft_memcpy.c
+++ libft.a/ft_memcpy.c
@@ -14,31 +14,16 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t			i;
-	unsigned char		*p;
-	unsigned char		*a;
-	size_t			sizedest;
-	size_t			sizesrc;
+	unsigned char	*p;
+	unsigned char	*a;
 
-	i = 0;
 	if (n == 0)
 		return (dest);
-	if (!src || !dest)
+	if (!src && !dest)
 		return (dest);
 	p = (unsigned char *) src;
 	a = (unsigned char *) dest;
-	sizedest = ft_strlen(dest);
-	sizesrc = ft_strlen(src);
-	if (sizedest < sizesrc)
-	{
-		while (i < n)
-		{
-			a[i] = p[i];
-			i++;
-		}
-		return (a);
-	}
-	while (n-- > 0)
+	while (n--)
 		a[n] = p[n];
 	return (a);
 }
--- libft.a/ft_strncmp.c
+++ libft.a/ft_strncmp.c
@@ -14,23 +14,23 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    size_t    i;
+	size_t	i;
 
-    i = 0;
-    while ((s1[i] || s2[i]) && (i < n))
-    {
-        if(ft_isascii(s1[i]) == 0)
-          return (1);
-        if (ft_isascii(s2[i]) == 0)
-          return (-1);
-        if (s1[i] < s2[i])
-            return (-1);
-        if (s1[i] > s2[i])
-            return (1);
-        else
-            i++;
-    }
-    return (0);
+	i = 0;
+	while ((s1[i] || s2[i]) && (i < n))
+	{
+		if (ft_isascii(s1[i]) == 0)
+			return (1);
+		if (ft_isascii(s2[i]) == 0)
+			return (-1);
+		if (s1[i] < s2[i])
+			return (-1);
+		if (s1[i] > s2[i])
+			return (1);
+		else
+			i++;
+	}
+	return (0);
 }
 /*
 int main(void)
